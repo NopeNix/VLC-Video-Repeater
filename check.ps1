3ac8d51d39319165d712e78e1d8e83850b3e6783
NopeNix <info@philipp-sponsel.de>
2023-04-04 22:56:13 +0200

    added timeout

--- check.ps1
+++ check.ps1
@@ -1,5 +1,5 @@
 try {
-    Invoke-WebRequest -Uri http://127.0.0.1:8080/ -Method Head -ErrorAction Stop
+    Invoke-WebRequest -Uri http://127.0.0.1:8080/ -TimeoutSec 5 -Method Get -ErrorAction Stop 
     exit 0
 }
 catch {
